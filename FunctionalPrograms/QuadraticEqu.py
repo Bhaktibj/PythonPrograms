7bbb36e7e1e9d0b2dbd226cc062bfa0f3ae0dc72
Bhaktibj <45263128+Bhaktibj@users.noreply.github.com>
2019-04-17 14:37:25 +0530

    Update QuadraticEqu.py

--- FunctionalPrograms/QuadraticEqu.py
+++ FunctionalPrograms/QuadraticEqu.py
@@ -1,7 +1,6 @@
 # Write a program Quadratic.
 # Python to find the roots of the equation a*x*x + b*x + c.
-# Since the equation is x*x, hence there are 2 roots.
-# The 2 roots of the equation can be found using a formula
+
 import math
 
 a = int(input("Enter The Input a"))
