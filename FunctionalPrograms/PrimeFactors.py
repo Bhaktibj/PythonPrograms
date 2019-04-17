d8ec390bb02595adc76c4edc601ea6f4e2cc70cb
Bhaktibj <45263128+Bhaktibj@users.noreply.github.com>
2019-04-17 14:38:34 +0530

    Update PrimeFactors.py

--- FunctionalPrograms/PrimeFactors.py
+++ FunctionalPrograms/PrimeFactors.py
@@ -1,5 +1,5 @@
 #  Number to find the prime factors
-# Logic -> Traverse till i*i <= N instead of i <= N for efficiency.
+
 number = int(input("Enter the Number"))
 for i in range(2, number, 1):
     while number % i == 0:
