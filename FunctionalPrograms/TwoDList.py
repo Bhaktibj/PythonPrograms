c1299c7838723cbc1df35efe829516a1cd613a11
Bhaktibj <45263128+Bhaktibj@users.noreply.github.com>
2019-04-24 18:27:03 +0530

    Update TwoDList.py

--- FunctionalPrograms/TwoDList.py
+++ FunctionalPrograms/TwoDList.py
@@ -1,13 +1,11 @@
-# 13th program
-# program to print the two dimensional list
 
-rows = int(input("Enter the rows\n"))
-col = int(input("Enter the columns\n"))
-a = [[0 for i in range(col)] for j in range(rows)]
+# program to print the two dimensional array or list
+
+rows = int(input("Enter row size \n"))
+col = int(input("Enter column size \n"))
+two_d_array = [[0 for i in range(col)] for j in range(rows)]
 print("Enter the Elements of the list")
 for i in range(rows):
     for j in range(col):  # loop for creating the list elements during run time
-        a[i][j] = int(input("Enter the Elements"))
-for i in range(rows):
-    for j in range(rows):  # loop for printing the  2d-list
-        print(a[i][j], end=" ")
+        tow_d_array[i][j] = int(input("Enter the Elements"))
+print(two_d_array)
