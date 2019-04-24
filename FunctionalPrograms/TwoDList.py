1f216047848ed79d660ecb8cfdf1249a12ad0e7b
Bhaktibj <45263128+Bhaktibj@users.noreply.github.com>
2019-04-24 18:28:12 +0530

    Update TwoDList.py

--- FunctionalPrograms/TwoDList.py
+++ FunctionalPrograms/TwoDList.py
@@ -7,5 +7,5 @@ two_d_array = [[0 for i in range(col)] for j in range(rows)]
 print("Enter the Elements of the list")
 for i in range(rows):
     for j in range(col):  # loop for creating the list elements during run time
-        tow_d_array[i][j] = int(input("Enter the Elements"))
+        tow_d_array[i][j] = int(input())
 print(two_d_array)
